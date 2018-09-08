78a671ed7c8fd841d21cb4d4adc0bcc808fde75e
Bintang Pratama <bp070798@gmail.com>
2018-09-08 23:24:00 +0700

    By Bintang

--- head.php
+++ head.php
@@ -8,7 +8,7 @@ $nama=$_SESSION['nama'];
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>OFON</title>
+<title>Whizpos</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="OFON E-Pengajuan" />
@@ -89,29 +89,28 @@ function delete_row(rowno)
 						
 <?php 
 include "function/db.php";
-$gid = $_SESSION['gid'];
-$prv=mysqli_query($db,"SELECT * FROM a_groupprivileges where group_id='$gid' AND permission='1'");
-while($prvl=mysqli_fetch_array($prv)){
-$ambilmenu=mysqli_query($db,"SELECT * FROM a_menu where id='$prvl[menu_id]'");
+$ambilmenu=mysqli_query($db,"SELECT * FROM a_menu where parrent_id='0'");
 while($menu=mysqli_fetch_array($ambilmenu)){
-?>
-
-						<li>
+ $prvl=mysqli_fetch_array(mysqli_query($db,"SELECT * from a_groupprivileges where menu_id=$menu[id]")); if ($prvl[permission]!='0') {
+		?>				<li>
 							<a href="<?php echo $menu[link]; ?>"><i class="<?php echo $menu[icon_code];?>"></i><?php echo $menu[menu]; if($menu[link]=='#'){?> <span class="fa arrow"></span></a>
 							<ul class="nav nav-second-level collapse">
 <?php								
 $ambilsubmenu=mysqli_query($db,"SELECT * FROM a_menu where parrent_id=$menu[id]");
 while($submenu=mysqli_fetch_array($ambilsubmenu)){
+$prvls=mysqli_fetch_array(mysqli_query($db,"SELECT * from a_groupprivileges where menu_id=$submenu[id]")); if ($prvls[permission]!=0) {
+
+
 ?>
 								<li>
 									<a href="<?php echo $submenu[link]; ?>"><?php echo $submenu[menu]; ?></a>
 								</li>
-							<?php } ?>	
+							<?php } } ?>	
 							</ul>
-						<?php } else {echo '</a>';} ?>
+						<?php }  else {echo '</a>';} ?>
 							<!-- /nav-second-level -->
 						</li>
-					<?php } } ?>
+					<?php } }?>
 					</ul>
 					<!-- //sidebar-collapse -->
 				</nav>
@@ -122,13 +121,13 @@ while($submenu=mysqli_fetch_array($ambilsubmenu)){
 		<div class="sticky-header header-section ">
 			<div class="header-left">
 				<!--toggle button start-->
-				<button id="showLeftPush"><i class="fa fa-bars"></i></button>
+				<button id="showLeftPush" style="background-color: #616468;"><i class="fa fa-bars"></i></button>
 				<!--toggle button end-->
 				<!--logo -->
 				<div class="logo">
 					<a href="#">
-						<h1>OFON</h1>
-						<span>AdminPanel</span>
+						<h1>Store 1</h1>
+						<span>tagline</span>
 					</a>
 				</div>
 				<!--//logo-->
@@ -137,60 +136,6 @@ while($submenu=mysqli_fetch_array($ambilsubmenu)){
 				<div class="clearfix"> </div>
 			</div>
 			<div class="header-right">
-				<div class="profile_details_left"><!--notifications of menu start -->
-					<ul class="nofitications-dropdown">
-						<li class="dropdown head-dpdn">
-
-
-
-<?php
-if($pos=='Head Of General Affair'){
-$sql="SELECT * FROM data_pengajuan where progres='2'";
-$sql2="SELECT count(*)  FROM data_pengajuan where progres='2'";
-} elseif ($pos=='Head Of Sales' OR $pos=='Head Of Marketing Communication' OR $pos=='Head Of Business Development' OR $pos=='Head Of Product Management' OR $pos=='Head Of Management Information System' OR $pos=='Head Of Operation' OR $pos=='Head Of Finance' ) {
-$sql="SELECT * FROM data_pengajuan where progres='1' AND unit_kerja='$_SESSION[divisi]'";
-$sql2="SELECT count(*) FROM data_pengajuan where progres='1' AND unit_kerja='$_SESSION[divisi]'";
-} else {
-	$sql="SELECT * FROM data_pengajuan where progres='5' AND unit_kerja='$_SESSION[divisi]'";
-$sql2="SELECT count(*)  FROM data_pengajuan where progres='5' AND unit_kerja='$_SESSION[divisi]'";
-} ; $b1 = mysqli_query($db,$sql);
-$sss=mysqli_query($db,$sql2);
-$asd=mysqli_fetch_array($sss);
-?>
-
-
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-envelope"></i><span class="badge"><?php echo $asd[0];  ?></span></a>
-							<ul class="dropdown-menu">
-								<li>
-									<div class="notification_header">
-										<?php if($asd==true){ ?>
-										<h3>You have <?php echo $asd[0]; ?> new messages</h3> <?php } else {  ?><h3>You have no messages</h3> <?php } ?>
-									</div>
-								</li>
-
-								<?php  while ( $b2=mysqli_fetch_array($b1)) { if ($b2['progres']!=='5') {
-									# code... ?>
-								<li><a href="index.php?page=detail&id=<?php echo $b2['form_code']; ?>">
-								   <div class="notification_desc">
-
-									<p>Ada Pengajuan <?php echo $b2['jenis_pengajuan']; ?> dari <?php echo $b2['created_by']; ?> </p>
-									<p><span><?php echo $b2['created_at']; ?></span></p>
-									</div>
-								   <div class="clearfix"></div>	
-								</a></li> <?php } else{ ?>      
-<li><a href="#">
-								   <div class="notification_desc">
-
-									<p>Ada Pembelanjaan <?php echo $b2['jenis_pengajuan']; ?> dari <?php echo $b2['created_by']; ?> </p>
-									<p><span><?php echo $b2['created_at']; ?></span></p>
-									</div>
-								   <div class="clearfix"></div>	
-								</a></li>
-								<?php } } ?>
-								</ul>
-					<div class="clearfix"> </div>
-				</div>
-				<!--notification menu end -->
 				<div class="profile_details">		
 					<ul>
 						<li class="dropdown profile_details_drop">
--- menu.sql
+++ menu.sql
@@ -12,22 +12,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for pengajuan
-CREATE DATABASE IF NOT EXISTS `pengajuan` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `pengajuan`;
+-- Dumping database structure for menu
+CREATE DATABASE IF NOT EXISTS `menu` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `menu`;
 
--- Dumping structure for table pengajuan.a_group
+-- Dumping structure for table menu.a_group
 CREATE TABLE IF NOT EXISTS `a_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table pengajuan.a_group: ~1 rows (approximately)
+-- Dumping data for table menu.a_group: ~0 rows (approximately)
 /*!40000 ALTER TABLE `a_group` DISABLE KEYS */;
+INSERT INTO `a_group` (`id`, `name`) VALUES
+	(2, 'Admin');
 /*!40000 ALTER TABLE `a_group` ENABLE KEYS */;
 
--- Dumping structure for table pengajuan.a_groupprivileges
+-- Dumping structure for table menu.a_groupprivileges
 CREATE TABLE IF NOT EXISTS `a_groupprivileges` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `group_id` int(11) NOT NULL DEFAULT '0',
@@ -38,24 +40,42 @@ CREATE TABLE IF NOT EXISTS `a_groupprivileges` (
   `update_by` varchar(50) NOT NULL DEFAULT '0',
   `update_at` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table pengajuan.a_groupprivileges: ~0 rows (approximately)
+-- Dumping data for table menu.a_groupprivileges: ~2 rows (approximately)
 /*!40000 ALTER TABLE `a_groupprivileges` DISABLE KEYS */;
+INSERT INTO `a_groupprivileges` (`id`, `group_id`, `menu_id`, `permission`, `created_by`, `created_at`, `update_by`, `update_at`) VALUES
+	(1, 1, 1, 1, 'Bintang', '05 September2018', '0', '0'),
+	(2, 1, 2, 1, 'Bintang', '05 September2018', '0', '0'),
+	(4, 1, 3, 1, 'Bintang', '05 September2018', '0', '0'),
+	(5, 1, 4, 1, 'Bintang', '05 September2018', '0', '0');
 /*!40000 ALTER TABLE `a_groupprivileges` ENABLE KEYS */;
 
--- Dumping structure for table pengajuan.a_menu
+-- Dumping structure for table menu.a_menu
 CREATE TABLE IF NOT EXISTS `a_menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `menu` varchar(50) NOT NULL DEFAULT '0',
   `parrent_id` varchar(50) NOT NULL DEFAULT '0',
-  `icon_code` varchar(50) NOT NULL DEFAULT '0',
+  `icon_code` varchar(50) DEFAULT NULL,
   `link` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COMMENT='Menu and submenu';
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COMMENT='Menu and submenu';
 
--- Dumping data for table pengajuan.a_menu: ~5 rows (approximately)
+-- Dumping data for table menu.a_menu: ~4 rows (approximately)
 /*!40000 ALTER TABLE `a_menu` DISABLE KEYS */;
+INSERT INTO `a_menu` (`id`, `menu`, `parrent_id`, `icon_code`, `link`) VALUES
+	(1, 'Dashboard', '0', 'fa fa-home nav_icon', 'index.php'),
+	(2, 'Masters', '0', 'fa fa-file  nav_icon', '#'),
+	(3, 'Setting User', '2', '0', 'user.php'),
+	(4, 'Setting Group', '2', '0', 'group.php'),
+	(5, 'Inventory', '0', 'fa fa-inventory  nav_icon', '#'),
+	(6, 'Table Panel', '0', 'fa store-alt  nav_icon', '#'),
+	(7, 'Front End', '0', 'fa fa-shopping-cart  nav_icon', '#'),
+	(8, 'Report', '0', 'fa fa-line-chart  nav_icon', '#'),
+	(9, 'System', '0', 'fa fa-setting  nav_icon', '#');
+/*!40000 ALTER TABLE `a_menu` ENABLE KEYS */;
+
+-- Dumping structure for table menu.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL DEFAULT '0',
@@ -68,10 +88,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_login` varchar(100) NOT NULL DEFAULT '0',
   `group_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table pengajuan.user: ~5 rows (approximately)
+-- Dumping data for table menu.user: ~0 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
